c7ce59c35ca044656e475c5780de826c31cf4330
Rick Hernandez <30671448+code2244@users.noreply.github.com>
2023-02-18 00:38:26 -0400

    json server created. Get request all working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios'
-
+import { useEffect, useState } from 'react'
+//
+export type Countries = {
+  "name": string,
+  "capital": string,
+  "subregion": string,
+  "region": string,
+  "population": number,
+  "demonym": string,
+}
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+  const [countries, setCountries] = useState<Countries[] | null>();
+  useEffect(() => {
+    const url = 'http://localhost:3001/countries'
+    axios.get(url).then((response) => {
+      setCountries(response.data)
+    })
+    // use empty dependency arr execute once the page load
+  }, []);
+  return <div className="App">{countries ? countries.map((countries) => {
+    return <p>{countries.name}</p> }) : null}</div>
 }
 
 export default App;
